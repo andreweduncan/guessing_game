1241473f4f38daf4f4a3a359f46cf14c7b4ea254
andreweduncan <andreweduncan@gmail.com>
2023-02-26 23:32:44 -0500

    refine guess code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
 
     println!("Please input your guess.");
 
@@ -12,4 +17,4 @@ fn main() {
         .expect("Failed to read line");
 
     println!("You guessed: {guess}");
-}
+}        
